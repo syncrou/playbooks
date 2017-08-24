d6f275bb13868a5e618623545be5397c2c11dd1d
Drew Bomhof <dbomhof@redhat.com>
2017-08-24 11:49:49 -0400

    Reworked debug
    1. Moved all separate pieces into include files
    2. Fixed the action plugin to correctly pass in a debug argument
    3. Added a dummy version of the manageiq_update_automate module

--- action_plugins/ansible_manageiq_api.py
+++ action_plugins/ansible_manageiq_api.py
@@ -8,9 +8,11 @@ import pprint
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
 
-        if 'debug' in task_vars:
+        if 'debug' in self._task.args:
             pp = pprint.PrettyPrinter(indent=4)
+            pp.pprint(self._task.args)
             pp.pprint(task_vars)
+            self._task.args.pop('debug', None)
 
 
         if task_vars is None:
@@ -19,17 +21,17 @@ class ActionModule(ActionBase):
         results = super(ActionModule, self).run(tmp, task_vars)
         # remove as modules might hide due to nolog
         #del results['invocation']['module_args']
-        for arg in ['miq_url', 'miq_username', 'miq_password']:
-            try:
-                if task_vars[arg]:
-                    module_vars[arg] = task_vars[arg]
-            except KeyError:
-                next
+        for arg in ['miq_url', 'miq_username', 'miq_password', 'miq_token']:
+            if arg in task_vars:
+                module_vars[arg] = task_vars[arg]
+
 
         results = merge_hash(
             results,
             self._execute_module(module_args=module_vars, task_vars=task_vars),
         )
+
+
         for field in ('_ansible_notify',):
             if field in results:
                 results.pop(field)
