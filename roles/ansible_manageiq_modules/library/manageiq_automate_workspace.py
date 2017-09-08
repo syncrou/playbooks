d3c639ba6733fead33411d83ce48adceb321201c
Drew Bomhof <dbomhof@redhat.com>
2017-09-08 17:57:26 -0400

    automate_workspace can set attributes on an object

--- roles/ansible_manageiq_modules/library/manageiq_automate_workspace.py
+++ roles/ansible_manageiq_modules/library/manageiq_automate_workspace.py
@@ -38,22 +38,37 @@ class Workspace(object):
         Object to modify and get the Workspace
     """
 
-    def __init__(self, workspace):
+    def __init__(self, workspace, connection):
         self._object = workspace
+        self._connection = connection
 
     def get(self):
         """
-        Return the current Workspace
+        Return the current Workspace object
         """
         return self._object['output']['workspace']
+
+    def set_attribute(self, action_dict):
+        """
+        Set the attribute called on the object with the passed in value
+        """
+        object_name = action_dict.keys()[0]
+        object, all = self._connection.get_object(object_name)
+        all['workspace']['root'][action_dict.keys()[0]] = action_dict.values()[0]
+
+        result = self._connection.set_object(all)
+
+        return dict(changed=False, msg=result)
+
 
 class StateVars(object):
     """
         Object to modify and get the StateVars
     """
 
-    def __init__(self, state_vars):
+    def __init__(self, state_vars, connection):
         self._object = state_vars
+        self._connection = connection
 
     def get(self):
         """
@@ -66,15 +81,13 @@ class ManageIQAutomateWorkspace(object):
         Object to execute automate workspace management operations in manageiq.
     """
 
-    def __init__(self, manageiq, guid, workspace_params, state_vars_params):
+    def __init__(self, manageiq, guid):
         self.manageiq = manageiq
 
         self.module = self.manageiq.module
         self.api_url = self.manageiq.api_url
         self.client = self.manageiq.client
         self._guid = guid
-        self._workspace = workspace_params
-        self._state_vars = state_vars_params
 
     def get_state_vars(self):
         """ Returns the current state_vars as looked up via the guid.
@@ -86,7 +99,7 @@ class ManageIQAutomateWorkspace(object):
         try:
             url = '%s/automate_workspaces/%s' % (self.api_url, self._guid)
             result = self.client.get(url)
-            state_vars = StateVars(result).get()
+            state_vars = StateVars(result, self).get()
         except Exception as e:
             self.module.fail_json(msg="failed to find the automate state_vars %s" % (str(e)))
 
@@ -102,11 +115,22 @@ class ManageIQAutomateWorkspace(object):
         try:
             url = '%s/automate_workspaces/%s' % (self.api_url, self._guid)
             result = self.client.get(url)
-            workspace = Workspace(result).get()
+            workspace = Workspace(result, self)
         except Exception as e:
             self.module.fail_json(msg="failed to find the automate workspace %s" % (str(e)))
 
-        return dict(changed=False, msg=workspace)
+        return workspace
+
+    def get_object(self, name):
+        url = '%s/automate_workspaces/%s' % (self.api_url, self._guid)
+        result = self.client.get(url)
+        return result['output']['workspace'][name], result['output']
+
+    def set_object(self, obj):
+        url = '%s/automate_workspaces/%s' % (self.api_url, self._guid)
+        result = self.client.post(url, action='edit', resource=obj)
+        return result
+
 
 
 
@@ -117,20 +141,24 @@ def main():
                 manageiq_connection=dict(required=True, type='dict',
                                          options=manageiq_argument_spec()),
                 guid=dict(required=True, type='str'),
-                workspace=dict(required=False, type='str'),
+                workspace=dict(required=False, type='dict'),
                 state_vars = dict(required=False, type='dict')
                 ),
             )
 
     guid = module.params['guid']
     workspace = module.params['workspace']
+    workspace_actions = workspace.keys()
     state_vars = module.params['state_vars']
 
     manageiq = ManageIQ(module)
-    manageiq_automate_workspace = ManageIQAutomateWorkspace(manageiq, guid, workspace, state_vars)
+    manageiq_automate_workspace = ManageIQAutomateWorkspace(manageiq, guid)
 
     if manageiq_automate_workspace:
-        res_args = manageiq_automate_workspace.get_workspace()
+        if workspace and workspace_actions:
+            for action in workspace_actions:
+                workspace_sandbox = manageiq_automate_workspace.get_workspace()
+                res_args = getattr(workspace_sandbox, action)(workspace[action])
     else:
         res_args = module.fail_json(msg="failed to return the automate workspace")
 
