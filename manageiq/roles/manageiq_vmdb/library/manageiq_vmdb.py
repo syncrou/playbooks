a1ddd0b29df2cf06752359f0b2e89357223d2b1d
Drew Bomhof <dbomhof@redhat.com>
2017-11-07 17:35:54 -0500

    Link vms to a service is working

--- manageiq/roles/manageiq_vmdb/library/manageiq_vmdb.py
+++ manageiq/roles/manageiq_vmdb/library/manageiq_vmdb.py
@@ -186,7 +186,7 @@ class Vmdb(ManageIQVmdb):
         if self.exists(action_string):
             data = self._module.params['data']
             result = self._client.post(self.post_url, action=action_string, resource=data)
-            if result['success'] == 'true':
+            if result['success']:
                 return dict(changed=False, value=result)
             return self._module.fail_json(msg=result['message'])
         return self._module.fail_json(msg="Action not found")
